ea99a7a33bc0039bdb7da000656134d282afdee6
Stean Kotze <steank@brownsjewellers.com>
2025-05-25 18:42:12 +0200

    removed stuff from telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,11 +13,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
             },
         })
 
-        -- Enable Telescope extensions if they are installed
-        pcall(require("telescope").load_extension, "fzf")
-        pcall(require("telescope").load_extension, "ui-select")
-
-        -- See `:help telescope.builtin`
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
         vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
@@ -27,25 +22,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
         vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
         vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
-        -- Slightly advanced example of overriding default behavior and theme
         vim.keymap.set("n", "<leader>/", function()
-            -- You can pass additional configuration to Telescope to change the theme, layout, etc.
             builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
                 winblend = 10,
                 previewer = false,
             }))
         end, { desc = "[/] Fuzzily search in current buffer" })
-
-        vim.keymap.set("n", "<leader>s/", function()
-            builtin.live_grep({
-                grep_open_files = true,
-                prompt_title = "Live Grep in Open Files",
-            })
-        end, { desc = "[S]earch [/] in Open Files" })
-
-        -- Shortcut for searching your Neovim configuration files
-        vim.keymap.set("n", "<leader>sn", function()
-            builtin.find_files({ cwd = vim.fn.stdpath("config") })
-        end, { desc = "[S]earch [N]eovim files" })
     end,
 }
